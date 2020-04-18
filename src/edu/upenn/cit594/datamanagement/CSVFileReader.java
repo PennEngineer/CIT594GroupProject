47e534f1cc16e9e3550d8cd9ad696336e333c357
phosid <phosidney@gmail.com>
2020-04-18 15:11:14 -0500

    updated CSVFileReader for Parking Violations

--- src/edu/upenn/cit594/datamanagement/CSVFileReader.java
+++ src/edu/upenn/cit594/datamanagement/CSVFileReader.java
@@ -9,16 +9,13 @@ import edu.upenn.cit594.data.ParkingViolationObject;
 public class CSVFileReader implements Reader {
 	
 	protected String inputFile;
-	protected String statesFile;
-	protected List<ParkingViolationObject> violationObjects = new ArrayList<ParkingViolationObject>();
-	
 	
 	public CSVFileReader(String inputFile) {
 		this.inputFile = inputFile;
 	}
-	
-	@Override
-	public List<ParkingViolationObject> getAllObjects() {
+
+	public ArrayList<ParkingViolationObject> getParkingViolationObjects() {
+		ArrayList<ParkingViolationObject> violationObjects = new ArrayList<>();
 		File inFile = new File(inputFile);
 		try {
 			Scanner in = new Scanner(inFile,"UTF-8");
