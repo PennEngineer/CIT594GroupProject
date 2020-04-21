86c74fb76e3a9f5df011af11ac9fb258b47a25ed
Kevin <Kevn@seas.upenn.edu>
2020-04-21 17:43:09 -0500

    main edit

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,57 +1,44 @@
 package edu.upenn.cit594;
 
-import java.io.File;
-import java.util.ArrayList;
 
-import edu.upenn.cit594.data.PopulationObject;
-import edu.upenn.cit594.data.Property;
+import java.io.File;
 import edu.upenn.cit594.datamanagement.CSVFileReader;
 import edu.upenn.cit594.datamanagement.CSVPropertyReader;
 import edu.upenn.cit594.datamanagement.PopulationFileReader;
-import edu.upenn.cit594.datamanagement.Reader;
-import edu.upenn.cit594.processor.MarketValueComparator;
 import edu.upenn.cit594.processor.Processor;
 
 public class Main {
-	
-	static String logName;
 
 	public static void main(String[] args) {
 	
-//		if (args.length != 5) {
-//			System.out.println("Please enter all 5 arguments.");
-//			System.exit(0);
-//		}
-//		if(!args[0].toLowerCase().equals("json") && !args[0].toLowerCase().equals("csv")) {
-//			System.out.println("Please specify either json or csv.");
-//			System.exit(0);
-//		}
-//		File input = new File(args[1]);
-//		if(!input.exists() || !input.canRead()) { 
-//		    System.out.println("File name: " + args[1] + " does not exist or cannot be read.");
-//		    System.exit(0);  
-//		}
-//		File input2 = new File(args[2]);
-//		if(!input2.exists() || !input2.canRead()) { 
-//		    System.out.println("File name: " + args[2] + " does not exist or cannot be read.");
-//		    System.exit(0);  
-//		}
-//		File input3 = new File(args[3]);
-//		if(!input3.exists() || !input3.canRead()) { 
-//		    System.out.println("File name: " + args[3] + " does not exist or cannot be read.");
-//		    System.exit(0);  
-//		}
-//		public Processor(Reader reader, ArrayList<PopulationObject> pop, ArrayList<Property> properties) {
-//		logName = args[4];
+		if (args.length != 5) {
+			System.out.println("Please enter all 5 arguments.");
+			System.exit(0);
+		}
+		if(!args[0].toLowerCase().equals("json") && !args[0].toLowerCase().equals("csv")) {
+			System.out.println("Please specify either json or csv.");
+			System.exit(0);
+		}
+		File input = new File(args[1]);
+		if(!input.exists() || !input.canRead()) { 
+		    System.out.println("File name: " + args[1] + " does not exist or cannot be read.");
+		    System.exit(0);  
+		}
+		File input2 = new File(args[2]);
+		if(!input2.exists() || !input2.canRead()) { 
+		    System.out.println("File name: " + args[2] + " does not exist or cannot be read.");
+		    System.exit(0);  
+		}
+		File input3 = new File(args[3]);
+		if(!input3.exists() || !input3.canRead()) { 
+		    System.out.println("File name: " + args[3] + " does not exist or cannot be read.");
+		    System.exit(0);  
+		}
+		
 		CSVFileReader reader = new CSVFileReader("properties.csv");
 		PopulationFileReader pop = new PopulationFileReader("population.txt");
 		CSVPropertyReader properties = new CSVPropertyReader("properties.csv");
 		Processor processor = new Processor(reader, pop.getPopulationObjects(), properties.getPropertyObjects());
-//		for(Property property : properties.getPropertyObjects()) {
-//			System.out.println(property.getZipCode());
-//		}
-		
-		System.out.println("Final Answer: " + processor.getAverage(new MarketValueComparator(), 19107));
 		
 	}
 
