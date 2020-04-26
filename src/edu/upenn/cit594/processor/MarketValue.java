1ddabf193c4150c2885f08c6b1ebf9ef8d251056
phosid <phosidney@gmail.com>
2020-04-26 17:00:13 -0500

    removed unused code

--- src/edu/upenn/cit594/processor/MarketValue.java
+++ src/edu/upenn/cit594/processor/MarketValue.java
@@ -1,14 +1,10 @@
 package edu.upenn.cit594.processor;
 
-import java.util.HashMap;
-import java.util.Map;
 import edu.upenn.cit594.data.Property;
 
 
 public class MarketValue implements Value {
 
-	private Map<Integer, Double> results = new HashMap<>();
-	
 	public String value(Property p) {
 		return p.getMarketValue();
 	}
