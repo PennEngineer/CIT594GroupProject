21fe7ebe79b01ee79546a358b9a83c0fa52a0f7a
phosid <phosidney@gmail.com>
2020-04-26 16:58:54 -0500

    refactored code for value classes and interface

--- src/edu/upenn/cit594/processor/Value.java
+++ src/edu/upenn/cit594/processor/Value.java
@@ -7,8 +7,7 @@ import edu.upenn.cit594.data.Property;
  * increasing the reusability of the code.
  */
 public interface Value {
-	
-//	double getAverage(ArrayList<Property> properties, int zipCode);
+
 	String value(Property p);
 
 }
